c4295a84ee64049231fb2f96343c5965e269cd98
Kush Patel <kush@kush.in>
2021-05-15 12:44:40 +0530

    Removed max-memory-restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     apps : [{
       name        : "Main",
-      script      : "./index.js",
-      max_memory_restart	: "600M"    
+      script      : "./index.js",  
     }]
   }
