d419a0c16640b782f50e99e15fbddc300480d4b3
Kush Patel <kush@kush.in>
2021-06-22 04:36:02 +0000

    added randomized fun facts in vote cmd

--- src/misc-cmds/vote.js
+++ src/misc-cmds/vote.js
@@ -1,5 +1,28 @@
 const { MessageEmbed } = require("discord.js");
 
+const FactList = [
+  {
+    msg: "The image in the embed was our first logo \n(attempt) Not too proud of that one.. :sweat_smile:",
+    thumbnail: "https://cdn.discordapp.com/attachments/804258629829918740/830025962329997362/2.png" 
+  },
+  {
+    msg: "Melody was first called 'nnoi plays' \n(Based on the username of our *honourable* founder).",
+    thumbnail: "https://cdn.discordapp.com/attachments/853976902994100224/856746348623691836/PicsArt_02-11-01.png"
+  },
+  {
+    msg: "There are just 2 employees at Melody, \nwhich are the founders and co-founders respectively.",
+    thumbnail: "https://cdn.discordapp.com/attachments/803882167193042975/812021018365394974/PINKmelody.png"
+  },
+  {
+    msg: "We initially struggled to find a way to keep our \nNodejs app running while we closed our terminal.",
+    thumbnail: "https://melody.pages.dev/assets/img/code-1839406_1920%20(2).jpg"
+  },
+  {
+    msg: "The owners of Melody are 15 year olds. \n(Seriously, not kidding)",
+    thumbnail: "https://cdn.discordapp.com/attachments/803882167193042975/812021018365394974/PINKmelody.png"
+  }
+];
+
 module.exports = {
   name: "vote",
   description: "To add/invite the bot to your server.",
@@ -19,12 +42,11 @@ module.exports = {
 
   // skipcq
   run: async (client, message, args) => {
+    const index = Math.floor(Math.random() * FactList.length + 1) - 1;
     const lolXD = new MessageEmbed()
       .setTitle("Vote For Melody")
       .setColor("GREEN")
-      .setThumbnail(
-        "https://cdn.discordapp.com/attachments/804258629829918740/830025962329997362/2.png"
-      )
+      .setThumbnail(FactList[index].thumbnail)
       .addField(
         "`Rewards coming soon! <3`",
         `\n[**' VOTE NOW! '**](https://top.gg/bot/809283972513267752/vote)`,
@@ -32,7 +54,7 @@ module.exports = {
       )
       .addField(
         "**Fun Fact:**",
-        `The image in the embed was our first logo \n(attempt) Not too proud of that one.. :sweat_smile:`
+        FactList[index].msg
       )
       .setFooter("Thanks for supporting Melody :)");
     return message.channel.send(lolXD);
@@ -50,12 +72,11 @@ module.exports = {
     ],
     // skipcq
     run: async (client, interaction, args) => {
+      const index = Math.floor(Math.random() * FactList.length + 1) - 1;
       const lolXD = new MessageEmbed()
         .setTitle("Vote For Melody")
         .setColor("GREEN")
-        .setThumbnail(
-          "https://cdn.discordapp.com/attachments/804258629829918740/830025962329997362/2.png"
-        )
+        .setThumbnail(FactList[index].thumbnail)
         .addField(
           "`Rewards coming soon! <3`",
           `\n[**' VOTE NOW! '**](https://top.gg/bot/809283972513267752/vote)`,
@@ -63,7 +84,7 @@ module.exports = {
         )
         .addField(
           "**Fun Fact:**",
-          `The image in the embed was our first logo \n(attempt) Not too proud of that one.. :sweat_smile:`
+          FactList[index].msg
         )
         .setFooter("Thanks for supporting Melody :)");
       return interaction.send(lolXD);
