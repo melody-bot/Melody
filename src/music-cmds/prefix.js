dcbc673f02735194a56c4416a99ec7bef65ee3cb
Kush Patel <kush@kush.in>
2021-06-24 05:26:14 +0000

    corrected prefix permission requirements

--- src/music-cmds/prefix.js
+++ src/music-cmds/prefix.js
@@ -4,7 +4,7 @@ module.exports = {
   usage: "",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
-    member: ["MANAGE_GUILD"],
+    member: [],
   },
   aliases: ["p"],
   /**
@@ -22,6 +22,24 @@ module.exports = {
       return message.channel.send(
         `This server's prefix is \`${getPrefix.prefix}\``
       );
+
+      function isPermitted() {
+        if (message.member.hasPermission(["MANAGE_GUILD"])) {
+          return true;
+        } else if (message.member.roles.cache.some((role) => role.name === "DJ")) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      let permission = isPermitted();
+
+      if (permission === false)
+        return client.sendError(
+          message.channel,
+            "Missing Permissions!\n You need the `DJ` role or `MANAGE_GUILD` permission to access this command."
+        );
 
     const newprefix = args[0]; // the provided argument. Ex: !changeprefix <newprefix>
     await client.setPrefix(message.guild.id, newprefix); // this will save the new prefix in the map and in the db to prevent multipy fetches
@@ -50,11 +68,32 @@ module.exports = {
 
     // skipcq
     run: async function (client, interaction, args) {
+      const guild = client.guilds.cache.get(interaction.guild_id);
+      const member = await guild.members.fetch(interaction.member.user.id);
+
+      function isPermitted() {
+        if (member.hasPermission(["MANAGE_GUILD"])) {
+          return true;
+        } else if (member.roles.cache.some((role) => role.name === "DJ")) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
       const getPrefix = await client.getPrefix(interaction.guild_id);
 
       if (!interaction.data.options)
         return interaction.send(
           `This server's prefix is ` + "`" + getPrefix.prefix + "`"
+        );
+
+      let permission = isPermitted();
+
+      if (permission === false)
+        return client.sendError(
+          interaction,
+            "Missing Permissions!\n You need the `DJ` or `MANAGE_GUILD` permission to role to access this command."
         );
 
       let newprefix = interaction.data.options[0].value; // the provided argument. Ex: !changeprefix <newprefix>
