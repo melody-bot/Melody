6b7f1547771410c5933e88f87e4410c48da1632b
Dhruv <officialdhruvinc@gmail.com>
2021-06-25 17:59:36 +0530

    Added message when bot joins a guild.

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,4 +1,12 @@
-// skipcq
+const { Discord, MessageEmbed } = require("discord.js")
+
 module.exports = async (client, guild) => {
   require("../util/slashCommands")(client, guild.id);
+
+    const embed = new MessageEmbed()
+    .setDescription("Thanks for inviting me! Use `.help` to get more info on all the commands.")
+
+    const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES' && 'VIEW_CHANNEL'))
+
+    channel.send(embed);
 };
--- src/music-cmds/help.js
+++ src/music-cmds/help.js
@@ -22,13 +22,11 @@ module.exports = {
     const embed = new MessageEmbed()
       .setAuthor(` ${client.user.username}`, `${client.config.IconURL}`)
       .setColor("343434")
-      .setDescription(
-        "Melody is a feature-rich music bot that comes with 24/7 and other\n premium features for free, invite Melody now! To get __all the information__\n about our bot please visit [our bot's wiki](https://github.com/noneedofit/Guides/wiki/Melody)\n\n`.play ['p']` - To play songs! You can also play a youtube video with its \nurl or just write the song name -` .p <song-name>`\n"
-      )
+      .setDescription("Melody is a feature-rich music bot that comes with 24/7 and other premium features for free,\ninvite Melody now! To get __all the information__\n about our bot please visit [our bot's wiki.](https://github.com/noneedofit/Guides/wiki/Melody)\n\n`.play ['p']` - To play songs! You can also play a youtube video with its url\nor just write the song name -` .p <song-name>`\n")
 
       .addField(
         "**IMPORTANT**",
-        `Those who don't find slash commands in their server is likely\n because of missing permissions. Please re-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
+        `Those who don't find slash commands in their server is likely because\nof missing permissions. Please re-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
           client.config.ClientID
         }&permissions=${
           client.config.Permissions
@@ -37,20 +35,19 @@ module.exports = {
 
       .addField(
         "HELP/SUPPORT",
-        `For any queries, doubts or any kind of bug reports you can join \nour [Support Server](${client.config.SupportServer})\nAnd any new major changes
-        being done to the bot will be updates in the\n change log we have in the .about cmd.`,
+        `For any queries, doubts or any kind of bug reports you can join our [Support Server](${client.config.SupportServer})\nAnd any new major changes being done to the bot will be updates in the\n change log we have in the .about cmd.`,
         false
       )
 
       .addField(
         "PATREON",
-        `You can support us to help us keep working on Melody,\n [click here to become a patreon](https://www.patreon.com/noneedofit)`,
+        `You can support us to help us keep working on Melody,\n[click here to become a patreon](https://www.patreon.com/noneedofit)`,
         false
       )
 
       .addField(
         "NOTE :-",
-        `In no way shall Melody bot be used to do anything that is against \ndiscord's [community guidelines.](https://discord.com/guidelines)`,
+        `In no way shall Melody bot be used to do anything that is against discord's [community guidelines.](https://discord.com/guidelines)`,
         true
       )
 
@@ -70,40 +67,36 @@ module.exports = {
       },
     ],
 
-    // skipcq
     run: async (client, interaction, args) => {
       const embed = new MessageEmbed()
         .setAuthor(` ${client.user.username}`, `${client.config.IconURL}`)
         .setColor("343434")
-        .setDescription(
-          "Melody is a feature-rich music bot that comes with 24/7 and other\n premium features for free, invite Melody now! To get __all the information__\n about our bot please visit [our bot's wiki](https://github.com/noneedofit/Guides/wiki/Melody)\n\n`.play ['p']` - To play songs! You can also play a youtube video with its \nurl or just write the song name -` .p <song-name>`\n"
-        )
-
+        .setDescription("Melody is a feature-rich music bot that comes with 24/7 and other premium features for free,\ninvite Melody now! To get __all the information__\n about our bot please visit [our bot's wiki.](https://github.com/noneedofit/Guides/wiki/Melody)\n\n`.play ['p']` - To play songs! You can also play a youtube video with its url\nor just write the song name -` .p <song-name>`\n")
+  
         .addField(
           "**IMPORTANT**",
-          `Those who don't find slash commands in their server is likely\n because of missing permissions. Please re-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
+          `Those who don't find slash commands in their server is likely because\nof missing permissions. Please re-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
             client.config.ClientID
           }&permissions=${
             client.config.Permissions
           }&scope=bot%20${client.config.Scopes.join("%20")})`
         )
-
+  
         .addField(
           "HELP/SUPPORT",
-          `For any queries, doubts or any kind of bug reports you can join \nour [Support Server](${client.config.SupportServer})\nAnd any new major changes
-        being done to the bot will be updates in the\n change log we have in the .about cmd.`,
+          `For any queries, doubts or any kind of bug reports you can join our [Support Server](${client.config.SupportServer})\nAnd any new major changes being done to the bot will be updates in the\n change log we have in the .about cmd.`,
           false
         )
-
+  
         .addField(
           "PATREON",
-          `You can support us to help us keep working on Melody,\n [click here to become a patreon](https://https:patreon.com/noneedofit)`,
+          `You can support us to help us keep working on Melody,\n[click here to become a patreon](https://www.patreon.com/noneedofit)`,
           false
         )
-
+  
         .addField(
           "NOTE :-",
-          `In no way shall Melody bot be used to do anything that is against \ndiscord's [community guidelines.](https://discord.com/guidelines)`,
+          `In no way shall Melody bot be used to do anything that is against discord's [community guidelines.](https://discord.com/guidelines)`,
           true
         )
 
