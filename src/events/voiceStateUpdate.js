4f003c92ffa29893534784424fe8ba8379cbbb19
Kush Patel <kush@kush.in>
2021-07-02 14:05:15 +0000

    pause music when no one in voice channel

---
+++ src/events/voiceStateUpdate.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = async (client, oldState, newState) => {
+  let player = client.Manager.get(oldState.guild.id);
+
+  const voiceChannel = client.channels.cache.get(oldState.channelID);
+
+  if (
+    newState.channel ||
+    oldState.channelID !== oldState.guild.me.voice.channelID
+  ) {
+    player = client.Manager.get(newState.guild.id);
+    if (!player) return;
+    if (player.paused === false) return;
+    player.pause(false);
+    const textChannel = client.channels.cache.get(player.textChannel);
+    const resumed = new MessageEmbed()
+      .setColor("GREEN")
+      .setDescription("Resumed the music!");
+    return textChannel.send(resumed);
+  }
+  if (voiceChannel.members.size === 1) {
+    player.pause(true);
+    const textChannel = client.channels.cache.get(player.textChannel);
+    const paused = new MessageEmbed()
+      .setDescription("Paused the music as nobody is in the voice channel")
+    textChannel.send(paused);
+  }
+};
