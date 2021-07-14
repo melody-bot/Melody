f54db390771f12a7913966b9e5c93fd9788a82ce
Kush Patel <kush@kush.in>
2021-07-14 16:59:14 +0000

    updated about slash command embed

--- src/misc-cmds/about.js
+++ src/misc-cmds/about.js
@@ -104,44 +104,54 @@ module.exports = {
 
       let uptime = `${days} days, ${hours} hours, \n${minutes} mins and ${seconds} secs`;
 
-      const Embed = new MessageEmbed()
+      let members = [];
 
-        .setAuthor(
-          "About Melody",
-          "https://cdn.discordapp.com/attachments/803882167193042975/812021018365394974/PINKmelody.png"
-        )
-        .setColor("ffefd5")
-        .setDescription(
-          "**__DEVELOPER INFO__**\n \nMelody bot is made and run by `noneedofit#5868` and\n `behind_the_hat_#2838`, for us Melody is our first discord.js project."
-        )
+      client.guilds.cache.forEach((g) => {
+        members.push(g.memberCount);
+      });
 
-        .addField(
-          "**IMPORTANT**",
-          `Those who don't find slash commands in their server is likely\n because of missing permissions. Please re-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
-            client.config.ClientID
-          }&permissions=${
-            client.config.Permissions
-          }&scope=bot%20${client.config.Scopes.join("%20")})`
-        )
+      const users = members.reduce((a, b) => a + b, 0);
 
-        .addField(
-          "Melody Stats:",
-          `Servers: **${client.guilds.cache.size}**\nUsers: **${client.users.cache.size}**\nChannels: **${client.channels.cache.size}**`,
-          true
-        )
-        .addField("Bot Uptime", `${uptime}`, true)
-        .addField(
-          "Bot Guide",
-          `[click here to read Melody bot wiki](https://www.github.com/noneedofit/Guides/wiki/Melody)`,
-          false
-        )
-        .addField(
-          "Change Log",
-          `- Slash commands integration and major embed changes.\n- Now you can vote Melody on top.gg, use .vote to vote, rewards coming soon!\n- You can now change the bot prefix using @Melody prefix <ur-prefix>, you can also check the server prefix by using \n<@809283972513267752> prefix\n- Now you require DJ role or few perms to
-            skip/clearqueue commands.`
-        );
+      client.database.model.countDocuments({}, function (err, count) {
+        if (err) return client.log(err);
+        const Embed = new MessageEmbed()
+          .setColor("ffefd5")
+          .addField(
+            "Notice Board",
+            `Those who don't find slash commands in their server\n is likely because of missing permissions. Please\nre-invite the bot using [this link](https://discord.com/oauth2/authorize?client_id=${
+              client.config.ClientID
+            }&permissions=${
+              client.config.Permissions
+            }&scope=bot%20${client.config.Scopes.join("%20")})`
+          )
 
-      return interaction.send(Embed).catch(() => interaction.send(""));
+          .addField(
+            "Melody Stats:",
+            `Servers: **${client.guilds.cache.size}**\nUsers: **${users}**\nSongs Played: **${count}**\nChannels: **${client.channels.cache.size}**`,
+            true
+          )
+          .addField("Bot Uptime", `${uptime}`, true)
+          .addField(
+            "Bot Guide",
+            `[Melody Wiki](https://www.github.com/melody-bot/Melody/wiki)\n[Developer Guide](https://github.com/melody-bot/Melody/wiki/For-Developers)\n[Website](https://melody-bot.tech)`,
+            true
+          )
+          .addField(
+            "Change Log",
+            `- The new \`history\` command shows guild history, and personal history\n with the \`personal\` argument.\n- Play multiple songs at a time by seperating them with \`;;\` operator.\n- Run multiple commands from a single message by seperating them\n with the \`&&\` operator.\n- Get information on any command using the \`--help\` flag.\n*Check Melody 3.0 [release notes](https://github.com/melody-bot/Melody/releases/tag/v3.0.0)*`
+          )
+          .addField(
+            "Developers",
+            "Melody bot is maintained by [Dhruv](https://github.com/noneedofit) and [Kush](https://github.com/git-kush).\nSupport Melody by becoming a [Patreon](https://patreon.com/noneedofit)"
+          )
+          .addField(
+            "License Notice",
+            "Melody is licensed under the [Apache-2.0 License](https://www.apache.org/licenses/LICENSE-2.0.txt)"
+          )
+          .setFooter("Copyright © Melody 2021", client.config.IconURL);
+
+        return interaction.send(Embed).catch(() => interaction.send(""));
+      });
     },
   },
 };
