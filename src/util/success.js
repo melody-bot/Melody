d35da425659f1d8f8610f3178ab5f127ad4d2b01
Kush Patel <kush@kush.in>
2021-07-28 10:35:55 +0000

    added util to send success embeds

---
+++ src/util/success.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require("discord.js");
+
+/**
+ * @param {String} text
+ * @param {TextChannel} channel
+ */
+
+module.exports = async (text, channel) => {
+  const embed = new MessageEmbed().setColor("GREEN").setDescription(text);
+  await channel.send(embed).catch((err) => client.log(err));
+};
