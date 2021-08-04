1057badca54daa3a4c860d228dbf8f1c424e48f1
Dhruv <officialdhruvinc@gmail.com>
2021-08-04 11:16:43 +0530

    buttons added, some issues to be resolved.

--- src/misc-cmds/invite.js
+++ src/misc-cmds/invite.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const disbut = require("discord-buttons");
 
 module.exports = {
   name: "invite",
@@ -20,6 +21,12 @@ module.exports = {
 
   // skipcq
   run: async (client, message, args) => {
+
+    const button = new disbut.MessageButton()
+     .setLabel("Invite Melody!")
+     .setUrl('https://discord.com/oauth2/authorize?client_id=809283972513267752&permissions=2163734592&scope=bot%20applications.commands')
+     .setStyle("url");
+
     let invite = new MessageEmbed()
       .setTitle(`Invite ${client.user.username}`)
       .setDescription(
@@ -32,8 +39,10 @@ module.exports = {
           client.config.Permissions
         }&scope=bot%20${client.config.Scopes.join("%20")}`
       )
-      .setColor("BLUE");
-    return message.channel.send(invite);
+      .setColor("BLUE")
+      .setFooter("You can also use the new button feature below!");
+
+    return message.channel.send(invite, button);
   },
 
   SlashCommand: {
@@ -48,6 +57,12 @@ module.exports = {
     ],
     // skipcq
     run: async (client, interaction, args) => {
+
+      const sbutton = new disbut.MessageButton()
+        .setLabel("Invite Melody!")
+        .setUrl('https://discord.com/oauth2/authorize?client_id=809283972513267752&permissions=2163734592&scope=bot%20applications.commands')
+        .setStyle("url");
+
       let invite = new MessageEmbed()
         .setTitle(`Invite ${client.user.username}`)
         .setDescription(
@@ -63,7 +78,7 @@ module.exports = {
           }${client.config.CallbackURL}&response_type=code`
         )
         .setColor("BLUE");
-      return interaction.send(invite);
+      return interaction.send(invite, sbutton);
     },
   },
 };
