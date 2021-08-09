c47a6232180e83d5c51c7baa4118d42a69805de1
Kush Patel <kush@kush.in>
2021-08-09 11:03:36 +0000

    removed auto-resume

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,27 +1,15 @@
 const { MessageEmbed } = require("discord.js");
-const sendSuccess = require("../util/success")
-module.exports = async (client, oldState, newState) => {
+module.exports = async (client, oldState) => {
   let player = client.Manager.get(oldState.guild.id);
 
   const voiceChannel = client.channels.cache.get(oldState.channelID);
 
-  if (
-    newState.channel ||
-    oldState.channelID !== oldState.guild.me.voice.channelID
-  ) {
-    player = client.Manager.get(newState.guild.id);
-    if (!player) return;
-    if (player.paused === false) return;
-    player.pause(false);
-    const textChannel = client.channels.cache.get(player.textChannel);
-    return sendSuccess(`Resumed the music!`, textChannel)
-  }
-  if (voiceChannel.members.size === 1) {
+  if (player && voiceChannel && voiceChannel.members.size === 1) {
     player.pause(true);
     const textChannel = client.channels.cache.get(player.textChannel);
     const paused = new MessageEmbed().setDescription(
       "Paused the music as nobody is in the voice channel"
     );
-    textChannel.send(paused);
+    return textChannel.send(paused);
   }
 };
